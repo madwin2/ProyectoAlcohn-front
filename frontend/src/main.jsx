788d2fbe9b85182142e0b630391bfb898d79f94f
madwin2 <madwin2@gmail.com>
2025-06-27 11:14:05 -0300

    stagewise

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { StagewiseToolbar } from '@stagewise/toolbar-react';
+import { ReactPlugin } from '@stagewise-plugins/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <StagewiseToolbar config={{ plugins: [ReactPlugin] }} />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
